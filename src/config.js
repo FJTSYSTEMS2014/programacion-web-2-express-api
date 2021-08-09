5791d6d1f32c06c803de33d85a2bf2f825825f19
Franck_Tscherig <fjtsystemsg@gmail.com>
2021-08-09 19:01:28 -0300

    faltaba el archivo de config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PORT: 3500,
+  JWT_SECRET: 'mysecret@',
+  DB_CONFIG: {
+    host: 'localhost',
+    user: 'franck',
+    password: '12345',
+    database: 'diplomatura',
+  },
+};
